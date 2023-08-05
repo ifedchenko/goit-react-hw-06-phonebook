6ee230cb6553701e0c902a658078c4759f41fb30
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-08-05 15:02:32 +0300

    Change App logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,74 +1,20 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Container } from './Container/Container';
-import { Section } from './Section/Section';
+import  Container  from './Container/Container';
+import  Section  from './Section/Section';
 import Form from './Form/Form';
 import Contact from './Contact/Contact';
 import Filter from './Filter/Filter';
 
 const App = () => {
-  const [contacts, setContacts] = useState([]);
-  const [filter, setFilter] = useState('');
-  const isFirstRender = useRef(true);
-
-  useEffect(() => {
-    const storedContacts = localStorage.getItem('contacts');
-    if (storedContacts) {
-      setContacts(JSON.parse(storedContacts));
-    } else {
-      setContacts([]);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (isFirstRender.current) {
-      isFirstRender.current = false;
-      return;
-    }
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const addContact = (id, name, number) => {
-    const isContactExist = contacts.some(
-      contact => contact.name.toLowerCase() === name.toLowerCase()
-    );
-
-    if (!isContactExist) {
-      setContacts(prevContacts => [{ id, name, number }, ...prevContacts]);
-    } else {
-      alert(`This ${name} already exists!`);
-    }
-  };
-
-  const deleteContact = id => {
-    const filterContacts = contacts.filter(contact => contact.id !== id);
-    setContacts(filterContacts);
-  };
-
-  const onFilterChange = e => {
-    setFilter(e.target.value.toLowerCase());
-  };
-
-  const onActiveFilter = () => {
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter)
-    );
-  };
+ 
   return (
     <>
       <Container>
         <Section title={'Phonebook'}>
-          <Form onSubmit={addContact}></Form>
+          <Form></Form>
         </Section>
         <Section title={'Contacts'}>
-          <Filter text={filter} onChange={onFilterChange} />
-          {filter === '' ? (
-            <Contact contacts={contacts} deleteContact={deleteContact} />
-          ) : (
-            <Contact
-              contacts={onActiveFilter()}
-              deleteContact={deleteContact}
-            />
-          )}
+          <Filter />
+         <Contact/>
         </Section>
       </Container>
     </>
