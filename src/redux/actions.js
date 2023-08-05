afa2d5d7cb3e0e496c711e1573a0b60620601d76
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-08-05 15:01:13 +0300

    add Redux base files

---
+++ src/redux/actions.js
@@ -0,0 +1,12 @@
+// import { createAction, nanoid } from "@reduxjs/toolkit";
+
+// export const addContactAction = createAction("contacts/addContact", contact => ({
+//   payload: {
+//     id: nanoid(),
+//     ...contact,
+//   },
+// }));
+
+// export const deleteContactAction = createAction("contacts/deleteContact");
+// export const filterContactsAction = createAction("contacts/filterContacts");
+
---
+++ src/redux/reducer.js
@@ -0,0 +1,34 @@
+// import { createReducer } from "@reduxjs/toolkit";
+// // import { addContactAction, deleteContactAction, filterContactsAction } from "./actions";
+
+
+
+// export const contactsReducer = createReducer(
+//   {
+//     name: 'contacts',
+//     initialState: {
+//       value: [],
+//     },
+//     addContactReducer: (state, action) => {
+//       state.value.unshift(action.payload);
+//     },
+//     deleteContactReducer: (state, action) => {
+//       state.value = state.value.filter(contact => contact.id !== action.payload);
+//     }
+//   }
+// );
+
+
+// export const filterReducer = createReducer(
+//   {
+//   name: 'filter',
+//   initialState: {
+//     value: '',
+//   filterContactReducer: (state, action) => {
+//     state.value = action.payload;
+//     }
+//   }
+// }
+// );
+
+
